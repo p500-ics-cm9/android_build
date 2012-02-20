96d297de29b92c7ac6e584c3eeb437eb6df4945a
p500-ics-cm9 <info@comvit.nl>
2012-02-20 19:49:54 +0100

    Update target/product/full_base.mk

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -34,7 +34,7 @@ PRODUCT_PACKAGES += \
 
 # Additional settings used in all AOSP builds
 PRODUCT_PROPERTY_OVERRIDES := \
-    ro.com.android.dateformat=MM-dd-yyyy \
+    ro.com.android.dateformat=dd-MM-yyyy \
     ro.config.ringtone=Ring_Synth_04.ogg \
     ro.config.notification_sound=pixiedust.ogg
 
